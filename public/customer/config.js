3b5e33e98bbe4479b343f8090a2556cd3e990902
magurophone <jansho6402@yahoo.co.jp>
2026-02-24 01:37:22 +0900

    管理画面から設定を更新

--- public/customer/config.js
+++ public/customer/config.js
@@ -48,8 +48,8 @@ window.DASHBOARD_CONFIG = {
     "googleFontsUrl": "https://fonts.googleapis.com/css2?family=Playfair+Display:wght@400;700;900&display=swap"
   },
   "images": {
-    "headerDesktop": "",
-    "headerMobile": "",
+    "headerDesktop": "https://drive.google.com/file/d/1WdK5vVT12q3Ub-wcTtoHXefQ2R0FsPms/view?usp=drivesdk",
+    "headerMobile": "https://drive.google.com/file/d/1gTu88yJXJY35TKPBvEZ3C4nntfs7OZXx/view?usp=drivesdk",
     "favicon": "./customer/vite.svg"
   },
   "sheets": {
