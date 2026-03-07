4d01207c5bf0491f8836a1b16847db57781cfcb6
magurophone <jansho6402@yahoo.co.jp>
2026-03-07 10:58:54 +0900

    管理画面から設定を更新

--- public/customer/config.js
+++ public/customer/config.js
@@ -184,19 +184,6 @@ window.DASHBOARD_CONFIG = {
         "imageUrl": "https://drive.google.com/file/d/1TxV7UZeCbnxBFT1rt4zJ5R0G9RD7G_9E/view?usp=drivesdk"
       },
       "accessKey": "oshitoyaka"
-    },
-    {
-      "key": "書",
-      "icon": "📝",
-      "columnIndex": 6,
-      "displayTemplate": "特典: secret",
-      "useKey": false,
-      "lockedContent": {
-        "imageUrl": "https://drive.google.com/file/d/1JA0-CLptJ-Xo4J-3mF9EkvbNKLs9OCBK/view?usp=drivesdk",
-        "text": "イベント来てくれてありがとう！イベント配布時のホムペアイコンの設定方法になります。わからないところは聞いてね！"
-      },
-      "accessKey": "777",
-      "isMembership": true
     }
   ],
   "home": {
